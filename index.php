76ac23f2567dc39a683c9eec6b8989e67e7ebbd2
mannuccimattia <mannuccimattia@gmail.com>
2025-07-21 17:42:47 +0200

    [UPD] movie class has multiple genres

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 
 class Genre
 {
-  public $genreName;
-  public function __construct($_genreName)
+  public $genres;
+  public function __construct($_gen_1, $_gen_2, $_gen_3)
   {
-    $this->genreName = $_genreName;
+    // $this->genreName = $_genreName;
+    $this->genres = [$_gen_1, $_gen_2, $_gen_3];
   }
 }
 class Movie
@@ -29,16 +30,23 @@ class Movie
   }
 }
 
-$mov_1 = new Movie("IT", "Andy Muschietti", 2017, "704.2 mil", "USA", new Genre("Horror"));
-$mov_2 = new Movie("The Blob", "Irvin S. Yeaworth Jr.", 1958, "4 mil", "USA", new Genre("Thriller"));
+$mov_1 = new Movie("IT", "Andy Muschietti", 2017, "704.2 mil", "USA", new Genre("Horror", "Supernatural", null));
+$mov_2 = new Movie("The Blob", "Irvin S. Yeaworth Jr.", 1958, "4 mil", "USA", new Genre("Thriller", "Horror", "Sci-Fi"));
 
 $movies = [$mov_1, $mov_2];
 
 foreach ($movies as $movie) {
   foreach ($movie as $key => $value) {
     if (!is_string($value) && !is_int($value)) {
-      foreach ($value as $objectValue) {
-        echo $objectValue, "<br>";
+      foreach ($value as $array) {
+        if (in_array(null, $array)) {
+          $callback = function ($item) {
+            return $item !== null;
+          };
+          echo join(", ", array_filter($array, $callback));
+        } else {
+          echo join(", ", $array);
+        }
       }
     } else {
       echo $value, "<br>";
