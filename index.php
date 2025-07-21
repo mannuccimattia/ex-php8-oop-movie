e1736005d5349b20a8c3dcb98e0bc792a7031c03
mannuccimattia <mannuccimattia@gmail.com>
2025-07-21 16:51:13 +0200

    [UPD] Show movies into index - raw

--- index.php
+++ index.php
@@ -22,6 +22,16 @@ class Movie
 $mov_1 = new Movie("IT", "Andy Muschietti", 2017, "704.2 mil", "USA");
 $mov_2 = new Movie("The Blob", "Irvin S. Yeaworth Jr.", 1958, "4 mil", "USA");
 
+$movies = [$mov_1, $mov_2];
 
-var_dump($mov_1);
-var_dump($mov_2);
+foreach ($movies as $movie) {
+  foreach ($movie as $key => $value) {
+
+    echo $value, "<br>";
+  }
+  echo '<hr>';
+}
+
+// var_dump($movies);
+// var_dump($mov_1);
+// var_dump($mov_2);
