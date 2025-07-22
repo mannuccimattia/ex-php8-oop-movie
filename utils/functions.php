900da6ade8ded6e696b9156b5067a2bedafecda1
mannuccimattia <mannuccimattia@gmail.com>
2025-07-22 14:23:40 +0200

    [UPD] Polished getMovies function

--- utils/functions.php
+++ utils/functions.php
@@ -1,50 +1,45 @@
 <?php
 
+require_once "./Traits/Cover.php";
+
+require_once "./Models/Genre.php";
+require_once "./Models/Movie.php";
+
+require_once "./data/movie_data.php";
+
+
+// convert movies to a php array
 function getMovies($movies)
 {
+
+  $moviesArray = [];
+
   foreach ($movies as $movie) {
+
+    $currentMovie = [];
 
     foreach ($movie as $key => $value) {
 
+      // convert genre to array, remove null values
       if ($key === "genre" && is_object($value)) {
-        // function: removes null values from genres array
+
         $callback = function ($item) {
           return $item !== null;
         };
+
         $genresArray = array_filter($value->genres, $callback);
-        echo join(", ", $genresArray), "<br>";
+        $currentMovie[$key] = join(", ", $genresArray);
       } else {
-        echo $value, "<br>";
+
+        $currentMovie[$key] = $value;
       }
     }
-    echo "<br>";
+
+    $moviesArray[] = $currentMovie;
   }
+
+  return $moviesArray;
 }
 
 // render cycle
-// foreach ($movies as $movie) {
-
-//   foreach ($movie as $key => $value) {
-
-//     // check if value is a class object (genre)
-//     if (!is_string($value) && !is_int($value)) {
-
-//       // cycle genres
-//       foreach ($value as $array) {
-
-//         if (in_array(null, $array)) {
-//           // function: removes null values from genres array
-//           $callback = function ($item) {
-//             return $item !== null;
-//           };
-//           // print genres
-//           return join(", ", array_filter($array, $callback)), "<br>";
-//         } else {
-//           return join(", ", $array), "<br>";
-//         }
-//       }
-//     } else { // if string or int, print
-//       return $value, "<br>";
-//     }
-//   }
-// }
+$moviesArray = getMovies($movies);
